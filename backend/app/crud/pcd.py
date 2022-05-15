d857986ec0a892a9c0bc1ccb4ed369023c3cfc57
Palm <supawichs@gmail.com>
2022-05-15 17:46:34 +0700

    Add pcd api and schemas

---
+++ backend/app/crud/pcd.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session ,load_only
+from app.models.pcd import PCD
+from app.schemas.pcd import PCDBase
+from datetime import date
+
+
+def add_pcd(request:PCDBase, db:Session):
+    db_pcd = PCD(pcd_lat = request.lat,
+                 pcd_long = request.long,
+                 pcd_date_aq = request.pcd_date_aq,
+                 pcd_pm25 = request.pcd_pm25,
+                 pcd_PM10 = request.pcd_PM10,
+                 pcd_aqi = request.pcd_aqi,
+                 pcd_ground = request.pcd_ground,
+                 pcd_mobile = request.pcd_mobile)
+    db.add(db_pcd)
+    db.commit()
+    db.refresh()
+
+def get_pcd_by_date(datetime:date, db:Session):
+    pass
+
+def get_pcd_by_range_date(from_date:date, til_date:date, db:Session):
+    pass
+
+def get_pm25_by_date(datetime:date, db:Session):
+    pass
