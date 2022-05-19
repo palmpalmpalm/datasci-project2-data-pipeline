329fe54ad895662d4abebb42dc7f0ff9c0fc3776
Palm <supawichs@gmail.com>
2022-05-20 01:36:45 +0700

    Add pipline dag

--- airflow/dags/pipline_dag.py
+++ airflow/dags/pipline_dag.py
@@ -3,11 +3,8 @@ from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 import os
-import subprocess
 import requests
 import random
-
-subprocess.call('pip3 install python-dotenv')
 from dotenv import load_dotenv
 
 
@@ -33,42 +30,42 @@ args = {
     'owner' : 'datasci-project2',
     'start_date' : datetime(2022, 5, 15)
 }
-
-
-dag = DAG(dag_id='pipline_dag', default_args=args, schedule_interval='*/1 * * * *', description='data pipeline for data science project 2',catchup=False) # run every 1 minute
-
-# def insert_data():
-#     url = 'http://host.docker.internal:8000/pcd/insert'
-#     data = {
-#         "pcd_lat": random.uniform(0, 1),
-#         "pcd_long": random.uniform(0, 1),
-#         "pcd_date_aq": None,
-#         "pcd_pm25": 1.3,
-#         "pcd_PM10": 0,
-#         "pcd_aqi": 0,
-#         "pcd_ground": True,
-#         "pcd_mobile": True
-#     }
-#     res = requests.post(url = url, json = data)
-#     print(res.json())
     
-# def get_data():
-#     url = 'http://host.docker.internal:8000/pcd/all'
-#     req = requests.get(url = url)
-#     print(len(req.json()))
+def trigger_scraper():
+    url = f"http://{SCRAPER_ENDPOINT}:{SCRAPER_PORT}" # end point path + "/"
+    req = requests.get(url)
+    print(req.json())
 
-def test():
-    print('hi')
+def trigger_cleansing():
+    url = f"http://{CLEANSING_ENDPOINT}:{CLEANSING_PORT}" # end point path + "/"
+    req = requests.get(url)
+    print(req.json())
+
+def trigger_predictor():
+    url = f"http://{PREDICTOR_ENDPOINT}:{PREDICTOR_PORT}" # end point path + "/"
+    req = requests.get(url)
+    print(req.json())
+
+dag = DAG(dag_id='pipline_dag', 
+          default_args=args, 
+          schedule_interval='*/1 * * * *', # run every 1 minute for hourly, use '@hourly'
+          description='Data Pipeline for Data Science Project 2', 
+          catchup=False) 
 
 with dag:
-    run_this_task = PythonOperator(
-        task_id = 'insert_random_data_to_db',
-        python_callable = test,    
+    scraping = PythonOperator(
+        task_id = 'Scrape data from websites and insert them to database',
+        python_callable = trigger_scraper,    
     )
     
-    run_this_task2 = PythonOperator(
-        task_id = 'get_all_data_from_db',
-        python_callable = test,
+    cleansing = PythonOperator(
+        task_id = 'Clean raw scraped data and insert them to database',
+        python_callable = trigger_cleansing,
     )
     
-    run_this_task >> run_this_task2
+    predicting = PythonOperator(
+        task_id = 'Predict future PM2.5 with cleaned data and insert them to database',
+        python_callable = trigger_predictor,
+    )
+    
+    scraping >> cleansing >> predicting
