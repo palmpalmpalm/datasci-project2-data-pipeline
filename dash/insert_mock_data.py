7c628afbf99e08b3d4f026dcea41986354778326
Suppanat Poo-im <suppanat.pm@gmail.com>
2022-05-20 00:31:52 +0700

    insert mock data

---
+++ dash/insert_mock_data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import requests
+import time
+import random
+import json
+
+
+def insert_data(i, name, lat, long):
+    url = 'http://localhost:8000/earthnull/insert'
+    timenow = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+    print(i, name)
+    data = {
+        "earthnull_timestamp": timenow,
+        "earthnull_station_id": i,
+        "earthnull_station_name": name,
+        "earthnull_region": name,
+        "earthnull_province": name,
+        "earthnull_lat": lat,
+        "earthnull_long": long,
+        "earthnull_pm25": random.randint(0, 100),
+        "earthnull_pm10": random.randint(0, 100),
+        "earthnull_wind_dir": random.randint(0, 365),
+        "earthnull_wind_speed": random.randint(0, 200),
+        "earthnull_RH": random.randint(0, 100)
+    }
+    res = requests.post(url=url, json=data)
+    print(res.json())
+
+
+if __name__ == '__main__':
+
+    name = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E"}
+    long = {1: 99.458001, 2: 103.439169,
+            3: 101.378487, 4: 100.523237, 5: 99.673471}
+    lat = {1: 18.104180, 2: 16.694087, 3: 12.769930, 4: 13.743236, 5: 7.888617}
+    while (True):
+        for i in range(1, 6):
+            insert_data(str(i), name[i], lat[i], long[i])
+        time.sleep(300)
