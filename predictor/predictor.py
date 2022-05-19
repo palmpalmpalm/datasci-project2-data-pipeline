eca343c32fe3d93b6734834cdaa8222545e30c36
Suppanat Poo-im <suppanat.pm@gmail.com>
2022-05-20 01:17:23 +0700

    edit predictor

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -19,39 +19,53 @@ API_URL = f"{API_ENDPOINT}:{API_PORT}"
 app = FastAPI()
 
 # Data schema
+
+
 class Data(BaseModel):
     demo_int: int
     demo_float: float
 
 # Demo model class
+
+
 class DemoModel:
     def __init__(self) -> None:
         # load weight here
         self.name = 'demo model'
-        
-    def predict(data:Data):
+
+    def predict(data: Data):
         # Real model should pass the data to the model
         return random.uniform(0, 1)
+
 
 # Init model
 demo_model = DemoModel()
 
 # insert prediction result to database
+
+
 def insert_data(data):
-    url = API_ENDPOINT + '/insert_prediction'
-    res = requests.post(url = url, json = data.json())
+    url = 'http://localhost:8000/predicted/insert'
+    res = requests.post(url=url, json=data.json())
     print(res.json())
-    
+
 # get latest data
-def get_latest_data():
-    url =  url = API_ENDPOINT + '/cleaned_data_latest'
-    req = requests.get(url = url)
+
+
+def get_latest_data(stationid):
+    url = 'http://localhost:8000/predicted/cleaned_earthnull/'+str(stationid)
+    req = requests.get(url=url)
     print(len(req.json()))
+    return req.json()
+
 
 # api for get lastest data -> inference the data -> insert prediction's result to database
-@app.post("/predictandpost")
-async def predictandpost(data: Data):
+@app.post("/predict-and-post")
+async def predict_and_post():
+    # for loop each station
     data = get_latest_data()
+    predicted = demo_model.predict(data)
+
+    insert_data(demo_model.predict(data))
+    #
     return insert_data(demo_model.predict(data))
-
-
