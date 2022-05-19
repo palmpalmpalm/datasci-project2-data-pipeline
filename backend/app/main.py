e7c1f3a3f85e110fa6d4764e2ef7c38eed3453dc
Palm <supawichs@gmail.com>
2022-05-19 16:57:36 +0700

    Comment unuse database

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,16 +9,16 @@ from .api import sfa
 from .api import traffic
 from .api import earthnull
 
-PCD.metadata.create_all(bind=engine)
-SFA.metadata.create_all(bind=engine)
-Traffic.metadata.create_all(bind=engine)
+# PCD.metadata.create_all(bind=engine)
+# SFA.metadata.create_all(bind=engine)
+# Traffic.metadata.create_all(bind=engine)
 EarthNull.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title='Data Sciene Project 2 Database Management')
 
-app.include_router(pcd.router)
-app.include_router(sfa.router)
-app.include_router(traffic.router)
+# app.include_router(pcd.router)
+# app.include_router(sfa.router)
+# app.include_router(traffic.router)
 app.include_router(earthnull.router)
 
